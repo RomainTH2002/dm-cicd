fc288479abc3ef6a25a763c0e5abd14b844b97e0
RomainTH2002 RTH <thiriot2002@gmail.com>
2025-01-21 16:40:01 +0000

    Ajout premier test create vehicule cli

---
+++ bin/cli.test.ts
@@ -0,0 +1,35 @@
+const axios = require('axios');
+const { createVehicle } = require('./path-to-your-script');
+const MockAdapter = require('axios-mock-adapter');
+
+// Création d'un mock d'axios
+const mock = new MockAdapter(axios);
+
+describe('createVehicle', () => {
+const address = 'http://localhost:3000'; // Exemple d'URL
+const shortcode = 'veh123';
+const battery = 80;
+const longitude = 2.3522;
+const latitude = 48.8566;
+
+afterEach(() => {
+mock.reset();
+});
+
+it('should create a vehicle successfully', async () => {
+// Réponse simulée réussie
+mock.onPost(`${address}/vehicles`).reply(200, {
+vehicle: { id: '12345' },
+});
+
+const consoleSpy = jest.spyOn(console, 'log').mockImplementation();
+
+await createVehicle(shortcode, battery, longitude, latitude, address);
+
+expect(consoleSpy).toHaveBeenCalledWith(
+'Created vehicle `veh123`, with ID `12345`'
+);
+
+consoleSpy.mockRestore();
+});
+});
